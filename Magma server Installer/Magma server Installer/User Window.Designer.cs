09dc89f5057e69b05f55c91b255a6e20b0e9ca77
12Acorns <nathanrutter09@gmail.com>
2023-05-07 18:15:43 +0100

    redo

---
+++ Magma server Installer/Magma server Installer/User Window.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace Magma_server_Installer
+{
+    partial class User_Window
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // User_Window
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1366, 729);
+            this.Name = "User_Window";
+            this.Text = "User_Window";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
